ccc2089c86e92f3fad025164f58ede9bc6777c4a
eliseto <esantos@xetechs.com>
2022-09-14 17:19:23 -0600

    [ADD] invoice_does_not_apply

---
+++ invoice_does_not_apply/models/sale_order.py
@@ -0,0 +1,12 @@
+from asyncore import write
+from odoo import api, fields, models,_
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    nothing_toinvoice = fields.Boolean(string="No aplica factura")
+
+    if nothing_toinvoice:
+        write({
+            'invoice_status':'no'
+        })
