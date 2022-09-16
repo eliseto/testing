4667fc54d80cfbb24d433b91f9b54fdbc5410e87
eliseto <esantos@xetechs.com>
2022-09-16 16:03:25 -0600

    FIXES

--- invoice_does_not_apply/models/sale_order.py
+++ invoice_does_not_apply/models/sale_order.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models,_
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    nothing_toinvoice = fields.Boolean(string="No aplica factura")
+    nothing_toinvoice = fields.Boolean(string="No aplica factura", default = False)
 
     if nothing_toinvoice:
         write({
