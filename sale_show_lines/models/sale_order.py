bf8aa0d44994dfa0c0b532d942fcc2f155104913
eliseto <esantos@xetechs.com>
2022-09-20 12:52:07 -0600

    campo no facturar

--- sale_show_lines/models/sale_order.py
+++ sale_show_lines/models/sale_order.py
@@ -11,9 +11,15 @@ class SaleOrderLine(models.Model):
     
 class SaleOrder(models.Model):
     _inherit='sale.order'
-
+    invoice_status = fields.Selection([
+        ('upselling', 'Upselling Opportunity'),
+        ('invoiced', 'Fully Invoiced'),
+        ('to invoice', 'To Invoice'),
+        ('not apply invoice', 'No aplica factura')
+        ('no', 'Nothing to Invoice')
+        ], string='Invoice Status', compute='_get_invoice_status', store=True)
     nothing_invoice = fields.Boolean(string="no facturar", default=False)
     def action_confirm(self):
         super().action_confirm()
         if self.nothing_invoice:
-            return {'invoice_status':'no'}
+            return {'invoice_status':'not apply invoice'}
