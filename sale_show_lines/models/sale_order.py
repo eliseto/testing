07a9ae6ba368c6913536b26f151c79b4f33dac87
eliseto <esantos@xetechs.com>
2022-09-20 17:59:37 -0600

    Fixes agregando nuevo estado

--- sale_show_lines/models/sale_order.py
+++ sale_show_lines/models/sale_order.py
@@ -11,22 +11,65 @@ class SaleOrderLine(models.Model):
     
 class SaleOrder(models.Model):
     _inherit='sale.order'
+
     invoice_status = fields.Selection([
         ('upselling', 'Upselling Opportunity'),
         ('invoiced', 'Fully Invoiced'),
         ('to invoice', 'To Invoice'),
         ('notinvoice', 'No aplica factura'),
         ('no', 'Nothing to Invoice')
-        ], string='Invoice Status', compute='_get_invoice_status', store=True, compute= 'compute_fields_nothing')
+        ], string='Invoice Status', compute='_get_invoice_status', store=True)
+
     nothing_invoice = fields.Boolean(string="No Facturar", default=False)
 
-    @api.depends('nothing_invoice')
-    def compute_fields_nothing(self):
-        for rec in self:
-            if rec.nothing_invoice:
-                rec.invoice_status =('notinvoice')
+    @api.depends('state', 'order_line.invoice_status','nothing_invoice')
+    def _get_invoice_status(self):
+        """
+        Compute the invoice status of a SO. Possible statuses:
+        - no: if the SO is not in status 'sale' or 'done', we consider that there is nothing to
+          invoice. This is also the default value if the conditions of no other status is met.
+        - to invoice: if any SO line is 'to invoice', the whole SO is 'to invoice'
+        - invoiced: if all SO lines are invoiced, the SO is invoiced.
+        - upselling: if all SO lines are invoiced or upselling, the status is upselling.
+        - notinvoice: if the 'do not invoice' button is true, it does not apply an invoice
+        """
+        unconfirmed_orders = self.filtered(lambda so: so.state not in ['sale', 'done'])
+        unconfirmed_orders.invoice_status = 'no'
+        confirmed_orders = self - unconfirmed_orders
+        if not confirmed_orders:
+            return
+        line_invoice_status_all = [
+            (d['order_id'][0], d['invoice_status'])
+            for d in self.env['sale.order.line'].read_group([
+                    ('order_id', 'in', confirmed_orders.ids),
+                    ('is_downpayment', '=', False),
+                    ('display_type', '=', False),
+                ],
+                ['order_id', 'invoice_status'],
+                ['order_id', 'invoice_status'], lazy=False)]
+
+        for order in confirmed_orders:
+            line_invoice_status = [d[1] for d in line_invoice_status_all if d[0] == order.id]
+            if order.state not in ('sale', 'done'):
+                order.invoice_status = 'no'
+            elif any(invoice_status == 'to invoice' for invoice_status in line_invoice_status):
+                order.invoice_status = 'to invoice'
+            elif line_invoice_status and all(invoice_status == 'invoiced' for invoice_status in line_invoice_status):
+                order.invoice_status = 'invoiced'
+            elif line_invoice_status and all(invoice_status in ('invoiced', 'upselling') for invoice_status in line_invoice_status):
+                order.invoice_status = 'upselling'
+            elif order.nothing_invoice:
+                order.invoice_status = 'notinvoice'
             else:
-                pass
+                order.invoice_status = 'no'
+
+    #@api.depends('nothing_invoice')
+    #def compute_fields_nothing(self):
+    #    for rec in self:
+    #        if rec.nothing_invoice:
+    #            rec.invoice_status =('notinvoice')
+    #        else:
+    #            pass
 
     #def action_confirm(self):
     #    super().action_confirm()
